942be37c3f5f8e44325a265a4c13ca6db65ca53f
mrchazaaa <charlie.howlett@live.co.uk>
2022-08-27 21:15:44 +0100

    rejig deploy scropt

--- PiServer/start.sh
+++
@@ -1,6 +0,0 @@
-# make sure that environment.json file is present and contains ThingsSpeak config keys and Google Sheets config keys
-
-pip3 install -r requirements.txt
-
-python3 /home/pi/workspace/arduino-monitoring/PiServer/PiServer.py
-
