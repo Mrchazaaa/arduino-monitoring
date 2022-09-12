4e928ce5aa9ed055dd6135bd621c3c2430187634
mrchazaaa <charlie.howlett@live.co.uk>
2022-09-12 23:26:39 +0100

    Update deploy script to include docker commands.

--- PiServer/deploy.sh
+++ PiServer/deploy.sh
@@ -1,11 +1,25 @@
 # make sure that environment.json file is present and contains ThingsSpeak config keys and Google Sheets config keys
 
+echo "installing dependencies"
+
 pip3 install -r requirements.txt
+
+echo "update lora receiver service"
 
 cp ./lora-receiver.service /etc/systemd/system
 
 systemctl daemon-reload
 
+echo "restart lora receiver service"
+
 systemctl stop lora-receiver.service
 systemctl start lora-receiver.service
 systemctl enable lora-receiver.service
+
+echo "killing loki container"
+
+docker kill loki-container
+
+echo "spinning up loki container"
+
+docker-compose up -d
